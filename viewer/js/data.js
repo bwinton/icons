b33cb3de58e0a058bbc11b00382ab79cd499314f
Travis CI <firefox-ux-team@mozilla.com>
2017-05-12 20:02:56 +0000

    Deploy a94a549e959f...10834553e28b to GitHub Pages.

---
+++ viewer/js/data.js
@@ -0,0 +1,221 @@
+var data = [
+  {
+    "name": "alert",
+    "items": [
+      "help.svg",
+      "info.svg",
+      "warning.svg"
+    ]
+  },
+  {
+    "name": "badges",
+    "items": [
+      "update-failed.svg",
+      "update-success.svg"
+    ]
+  },
+  {
+    "name": "branding",
+    "items": [
+      "firefox.svg",
+      "nightly.svg"
+    ]
+  },
+  {
+    "name": "content",
+    "items": [
+      "character-encoding.svg",
+      "copy.svg",
+      "cut.svg",
+      "find.svg",
+      "fullscreen.svg",
+      "open.svg",
+      "paste.svg",
+      "print.svg",
+      "save.svg",
+      "zoom-in.svg",
+      "zoom-out.svg"
+    ]
+  },
+  {
+    "name": "developer",
+    "items": [
+      "add.svg",
+      "app.svg",
+      "clear.svg",
+      "close.svg",
+      "command-console.svg",
+      "command-eyedropper.svg",
+      "command-frames.svg",
+      "command-measure.svg",
+      "command-noautohide.svg",
+      "command-paintflashing.svg",
+      "command-pick.svg",
+      "command-responsivemode.svg",
+      "command-rulers.svg",
+      "command-screenshot.svg",
+      "commandline-icon.svg",
+      "console-input.svg",
+      "console-output.svg",
+      "debugger-step-in.svg",
+      "debugger-step-out.svg",
+      "debugger-step-over.svg",
+      "debugging-devices.svg",
+      "debugging-workers.svg",
+      "developer-panel.svg",
+      "developer.svg",
+      "diff.svg",
+      "dir-close.svg",
+      "dir-open.svg",
+      "dock-bottom.svg",
+      "dock-side.svg",
+      "dock-undock.svg",
+      "fast-forward.svg",
+      "filter.svg",
+      "geometry-editor.svg",
+      "globe.svg",
+      "import.svg",
+      "item-toggle.svg",
+      "pane-collapse.svg",
+      "pane-expand.svg",
+      "pause.svg",
+      "play.svg",
+      "power.svg",
+      "promise-debugger.svg",
+      "pseudo-class.svg",
+      "rewind.svg",
+      "rotate-viewport.svg",
+      "search.svg",
+      "tool-canvas.svg",
+      "tool-debugger.svg",
+      "tool-dom.svg",
+      "tool-inspector.svg",
+      "tool-memory.svg",
+      "tool-network.svg",
+      "tool-options.svg",
+      "tool-profiler.svg",
+      "tool-scratchpad.svg",
+      "tool-shadereditor.svg",
+      "tool-storage.svg",
+      "tool-styleeditor.svg",
+      "tool-webaudio.svg",
+      "tool-webconsole.svg",
+      "touch-events.svg",
+      "webide.svg"
+    ]
+  },
+  {
+    "name": "general",
+    "items": [
+      "addon.svg",
+      "mail.svg",
+      "privacy.svg",
+      "quit.svg",
+      "settings-mac.svg",
+      "settings.svg",
+      "share.svg",
+      "sync.svg"
+    ]
+  },
+  {
+    "name": "media",
+    "items": [
+      "caption.svg",
+      "cast.svg",
+      "chapter.svg",
+      "fullscreen.svg",
+      "pause.svg",
+      "play.svg",
+      "rewind.svg",
+      "sound-muted.svg",
+      "sound.svg",
+      "unfullscreen.svg"
+    ]
+  },
+  {
+    "name": "navigation",
+    "items": [
+      "back-large.svg",
+      "back-secondary.svg",
+      "back.svg",
+      "chevron.svg",
+      "close.svg",
+      "forget.svg",
+      "forward-secondary.svg",
+      "forward.svg",
+      "go.svg",
+      "home.svg",
+      "magnifying-glass.svg",
+      "menu.svg",
+      "new-tab.svg",
+      "new-window.svg",
+      "private-browsing.svg",
+      "reload-small.svg",
+      "reload.svg",
+      "sidebars.svg",
+      "stop-small.svg",
+      "stop.svg",
+      "tab-groups.svg",
+      "tab-thin.svg",
+      "tab.svg"
+    ]
+  },
+  {
+    "name": "permissions",
+    "items": [
+      "camera-disable.svg",
+      "camera.svg",
+      "location-disable-mac.svg",
+      "location-disable-win.svg",
+      "location-disable.svg",
+      "location-mac.svg",
+      "location-win.svg",
+      "location.svg",
+      "microphone-disable.svg",
+      "microphone.svg",
+      "notifications-disable.svg",
+      "notifications.svg",
+      "password.svg",
+      "permissions.svg",
+      "plugin-disable.svg",
+      "plugin.svg",
+      "pointer-lock-disable.svg",
+      "pointer-lock.svg",
+      "popups.svg",
+      "screen-share-disable.svg",
+      "screen-share.svg",
+      "store-data-disable.svg",
+      "store-data.svg"
+    ]
+  },
+  {
+    "name": "places",
+    "items": [
+      "bookmark-starred.svg",
+      "bookmark.svg",
+      "bookmarks-menu.svg",
+      "download.svg",
+      "feed.svg",
+      "history.svg",
+      "import-export.svg",
+      "list.svg",
+      "organize.svg",
+      "search-history.svg",
+      "tab-crashed.svg",
+      "welcome-back.svg"
+    ]
+  },
+  {
+    "name": "reader",
+    "items": [
+      "exit.svg",
+      "narrate-faster.svg",
+      "narrate-slower.svg",
+      "narrate.svg",
+      "pocket.svg",
+      "reader-view.svg",
+      "type-controls-serif.svg",
+      "type-controls.svg"
+    ]
+  }
+]
